eb8139bb2261e616a9b20ec8a13d93277d893206
Stefan <stefandjuric@elfak.rs>
2021-04-12 10:16:20 +0200

    drugi lab

---
+++ Stefan/MyPLaces/app/src/main/java/ime/mosis/elfak/myplaces/MyPlacesList.java
@@ -0,0 +1,73 @@
+package ime.mosis.elfak.myplaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MyPlacesList extends AppCompatActivity {
+
+    ArrayList<String> places;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_places_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        places=new ArrayList<String>();
+        places.add("Tvrdjava");
+        places.add("Cair");
+        places.add("Park Svetog Save");
+        places.add("Trg Kralja Milana");
+        ListView myPlacesList=(ListView)findViewById(R.id.my_places_list_item);
+        myPlacesList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,places));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_my_places_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if(id==R.id.show_map_item){
+            Toast.makeText(this,"Show Map!", Toast.LENGTH_SHORT).show();
+        } else if(id==R.id.new_place_item){
+            Toast.makeText(this,"New Place!", Toast.LENGTH_SHORT).show();
+        } else if(id==R.id.about_item){
+//                Toast.makeText(this,"About!", Toast.LENGTH_SHORT).show();
+            Intent i=new Intent(this, About.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
